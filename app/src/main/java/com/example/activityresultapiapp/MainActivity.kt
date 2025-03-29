32910342971dcceb078e16052f4b5875e8d4d8a8
avlnkov <alex.lenkov@mail.ru>
2025-03-29 17:00:04 +0300

    есть проблема с получением Image интента

--- app/src/main/java/com/example/activityresultapiapp/MainActivity.kt
+++ app/src/main/java/com/example/activityresultapiapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.ComponentCaller
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -35,7 +36,11 @@ class MainActivity : AppCompatActivity() {
                 input: Intent
             ): Intent {
                 // Создаём Intent для получения результата
-                return input
+                log("create intent: ${input.type}")
+                if (input.type == INTENT_TYPE_IMAGE) {
+                    return input
+                }
+                    return input
             }
 
             // В случае когда прилетит результат будет вызван данный метод
@@ -44,6 +49,9 @@ class MainActivity : AppCompatActivity() {
                 intent: Intent?
             ): String? {
                 // Когда задача будет выполнена, то сюда прилетит resultCode и intent с данными
+
+                // todo Проблема в том, что сюда уже приходит null объект
+                log("parse intent: ${intent?.type}")
 
                 if (resultCode == RESULT_OK) {
                     val username =
@@ -59,7 +67,8 @@ class MainActivity : AppCompatActivity() {
 
         // Создаём лончер, где мы подписываемся на задачу и когда она будет выполнена, то сработает коллбэк
         // Передаём в него наш контракт и создаём коллбэк, Сюда прилетит строка
-        val launcher = registerForActivityResult(contract) {
+        val launcher = registerForActivityResult(contract) { it ->
+            log("contract value: $it")
             if (!it.isNullOrBlank()) { // Если строка не пустая и не равна null
                 usernameTextView.text = it
             }
@@ -68,41 +77,18 @@ class MainActivity : AppCompatActivity() {
 
 
         getUsernameButton.setOnClickListener {
-
             launcher.launch(UsernameActivity.newIntent(this))
-
         }
+
         getImageButton.setOnClickListener {
             // Это неявный интент на открытие галереи
-            Intent(Intent.ACTION_PICK).apply {
-                type = "image/*" // MIME types - указываем какой тип хотим, чтобы был в интенте
-                startActivityForResult(this, RC_IMAGE)
+            val intentForImage = Intent(Intent.ACTION_PICK).apply {
+                type =
+                    INTENT_TYPE_IMAGE // MIME types - указываем какой тип хотим, чтобы был в интенте
             }
-            //TODO get image
-        }
-    }
 
-    // Метод, который позволяет получить данные из другой Активити
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        intent: Intent?,
-        caller: ComponentCaller
-    ) {
-        super.onActivityResult(requestCode, resultCode, intent, caller)
+            launcher.launch(intentForImage)
 
-        if (resultCode == RESULT_OK) {
-            when (requestCode) {
-                RC_USERNAME -> {
-                    val username = intent?.getStringExtra(UsernameActivity.EXTRA_USERNAME) ?: ""
-                    usernameTextView.text = username
-                }
-
-                RC_IMAGE -> {
-                    val imageUri = intent?.data
-                    imageFromGalleryImageView.setImageURI(imageUri)
-                }
-            }
         }
     }
 
@@ -116,5 +102,10 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val RC_USERNAME = 100
         private const val RC_IMAGE = 101
+        private const val INTENT_TYPE_IMAGE = "image/*"
+
+        fun log(text: String) {
+            Log.d("MainActivity", text)
+        }
     }
 }
