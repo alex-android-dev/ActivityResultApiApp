8ed8b292b03a2b1a66cd411a7395ce84cb90764d
avlnkov <alex.lenkov@mail.ru>
2025-03-29 12:23:22 +0300

    create project

--- app/src/main/java/com/example/activityresultapiapp/MainActivity.kt
+++ app/src/main/java/com/example/activityresultapiapp/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.activityresultapiapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var getUsernameButton: Button
+    private lateinit var usernameTextView: TextView
+    private lateinit var getImageButton: Button
+    private lateinit var imageFromGalleryImageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        initViews()
+
+        getUsernameButton.setOnClickListener {
+            UsernameActivity.newIntent(this).apply {
+                startActivity(this)
+            }
+
+            //TODO get username
         }
+        getImageButton.setOnClickListener {
+            //TODO get image
+        }
+    }
+
+    private fun initViews() {
+        getUsernameButton = findViewById(R.id.get_username_button)
+        usernameTextView = findViewById(R.id.username_textview)
+        getImageButton = findViewById(R.id.get_image_button)
+        imageFromGalleryImageView = findViewById(R.id.image_from_gallery_imageview)
     }
 }
