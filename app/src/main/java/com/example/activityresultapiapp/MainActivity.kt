453745e1031a45df2a9909352d609931d92b8e75
avlnkov <alex.lenkov@mail.ru>
2025-03-29 13:22:29 +0300

    создали contract между Активити

--- app/src/main/java/com/example/activityresultapiapp/MainActivity.kt
+++ app/src/main/java/com/example/activityresultapiapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.activityresultapiapp
 
 import android.app.ComponentCaller
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +22,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
+
+
+        // Это контракт. Говорит о том, что для запуска UsernameActivity нам понадобится Intent
+        // Этот интент мы передадим, когда будем запускать задачу на выполнение
+        // Также говорим, что от запускаемой Активити нам понадобится Строка String
+        val contract = object : ActivityResultContract<Intent, String?>() {
+
+            // Система запустит Активити и будет ожидать результата
+            override fun createIntent(
+                context: Context,
+                input: Intent
+            ): Intent {
+                // Создаём Intent для получения результата
+                return input
+            }
+
+            // В случае когда прилетит результат будет вызван данный метод
+            override fun parseResult(
+                resultCode: Int,
+                intent: Intent?
+            ): String? {
+                // Когда задача будет выполнена, то сюда прилетит resultCode и intent с данными
+
+                if (resultCode == RESULT_OK) {
+                    val username =
+                        intent?.getStringExtra(UsernameActivity.EXTRA_USERNAME) ?: ""
+                    return username
+                }
+
+                // Если resultCode не ОК
+                return null
+
+            }
+        }
+        // Указываем 2 типа -
+        // 1. Какие данные понадобятся для запуска Активити (в нашем случае мы передаём интент)
+        // 2. Какие данные будем получать (В нашем случае это строка String)
 
         getUsernameButton.setOnClickListener {
 
